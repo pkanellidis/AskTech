a4f06be546d473c1f8d9d5b77edb74cdda657929
pkanellidis <pkanellidis@gitlab.com>
2025-04-15 01:55:34 +0300

    Make links draftable

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,17 +1,27 @@
 import TwitterIcon from '@/components/icons/TwitterIcon'
 import FacebookIcon from '@/components/icons/FacebookIcon'
+import type { HTMLAttributes } from 'astro/types'
+
+export interface SocialNetwork {
+	name: string
+	url: string
+	icon: HTMLAttributes<any>
+	draft: boolean
+}
 
 // ADD YOUR SOCIAL NETWORKS HERE
-export const SOCIALNETWORKS = [
+export const SOCIALNETWORKS: SocialNetwork[] = [
 	{
 		name: 'Facebook',
 		url: '#',
-		icon: FacebookIcon
+		icon: FacebookIcon,
+		draft: true
 	},
 
 	{
 		name: 'Twitter',
 		url: '#',
-		icon: TwitterIcon
+		icon: TwitterIcon,
+		draft: true
 	}
 ] as const
