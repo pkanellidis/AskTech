7d36ca95cb66e4311a9fa35acbfdd3546edab6fa
pkanellidis <pkanellidis@gitlab.com>
2025-05-21 01:09:00 +0300

    Start implementing post filtering per language

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,7 @@
 import { defineCollection, z } from 'astro:content'
 import { CATEGORIES } from '@/data/categories'
 
-const blog = defineCollection({
+const blogGR = defineCollection({
 	// Type-check frontmatter using a schema
 	schema: ({ image }) =>
 		z.object({
@@ -19,7 +19,25 @@ const blog = defineCollection({
 		})
 })
 
-export const collections = { blog }
+const blogEN = defineCollection({
+	// Type-check frontmatter using a schema
+	schema: ({ image }) =>
+		z.object({
+			title: z.string().max(80),
+			description: z.string(),
+			// Transform string to Date object
+			pubDate: z
+				.string()
+				.or(z.date())
+				.transform((val) => new Date(val)),
+			heroImage: image(),
+			category: z.enum(CATEGORIES),
+			tags: z.array(z.string()),
+			draft: z.boolean().default(false)
+		})
+})
+
+export const collections = { 'blog/en': blogEN, 'blog/gr': blogGR }
 
 //TODO: Translations using i18n and categories
 // Social Sharing
--- src/utils/post.ts
+++ src/utils/post.ts
@@ -2,7 +2,13 @@ import { getCollection } from 'astro:content'
 import { CATEGORIES } from '@/data/categories'
 
 export const getCategories = async () => {
-	const posts = await getCollection('blog')
+	const categoriesEN = await getCategoriesByLocale('en')
+	const categoriesGR = await getCategoriesByLocale('gr')
+	return [...categoriesEN, ...categoriesGR]
+}
+
+export const getCategoriesByLocale = async (locale: 'en' | 'gr') => {
+	const posts = await getCollection(locale)
 	const categories = new Set(
 		posts?.filter((post) => !post.data.draft).map((post) => post.data.category)
 	)
@@ -12,14 +18,24 @@ export const getCategories = async () => {
 }
 
 export const getPosts = async (max?: number) => {
-	return (await getCollection('blog'))
+	return [...(await getPostsByLocale('en', max)), ...(await getPostsByLocale('gr', max))]
+}
+
+export const getPostsByLocale = async (locale: 'en' | 'gr', max?: number) => {
+	return (await getCollection(locale))
 		?.filter((post) => !post.data.draft)
 		?.sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf())
 		.slice(0, max)
 }
 
 export const getTags = async () => {
-	const posts = await getCollection('blog')
+	const tagsEN = await getTagsByLocale('en')
+	const tagsGR = await getTagsByLocale('gr')
+	return [...tagsEN, ...tagsGR]
+}
+
+export const getTagsByLocale = async (locale: 'en' | 'gr') => {
+	const posts = await getCollection(locale)
 	const tags = new Set()
 	posts
 		?.filter((post) => !post.data.draft)
@@ -34,8 +50,8 @@ export const getTags = async () => {
 	return Array.from(tags)
 }
 
-export const getPostByTag = async (tag: string) => {
-	const posts = await getPosts()
+export const getPostByTag = async (locale: 'en' | 'gr', tag: string) => {
+	const posts = await getPosts(locale)
 	const lowercaseTag = tag.toLowerCase()
 	return posts
 		?.filter((post) => !post.data.draft)
@@ -44,8 +60,8 @@ export const getPostByTag = async (tag: string) => {
 		})
 }
 
-export const filterPostsByCategory = async (category: string) => {
-	const posts = await getPosts()
+export const filterPostsByCategory = async (locale: 'en' | 'gr', category: string) => {
+	const posts = await getPosts(locale)
 	return posts
 		?.filter((post) => !post.data.draft)
 		?.filter((post) => post.data.category.toLowerCase() === category)
